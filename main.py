b938ebf4808ba11ce31608e3138b990f73851ff1
Alek Mammadov <mamedxanli@gmail.com>
2017-04-13 19:27:44 +0200

    first

--- main.py
+++ main.py
@@ -2,44 +2,38 @@
 
 import abc
 
-class GetterSetter(object):
-    __metaclass__=abc.ABCMeta
-
-    @abc.abstractmethod
-    def set_val(self, input):
-        """Set a value in the instance"""
-        return
-    
-    @abc.abstractmethod
-    def get_val(self):
-        """Get and return a value from the instance"""
-        return
-
-class MyClass(GetterSetter):
-    def set_val(self, input):
-        self.val = input
-
+class GetSetParent(object):
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, value):
+        self.val = 0
+    def set_val(self, value):
+        self.val = value
     def get_val(self):
         return self.val
-
-x = MyClass()
-print x
-
-
+    @abc.abstractmethod
+    def showdoc(self):
+        return
 
 
+class GetSetInt(GetSetParent):
+    def set_val(self, value):
+        if not isinstance(self, int):
+            value = 0
+        super(GetSetInt, self).set_val(value)
+    def showdoc(self):
+        print('GetSetInt object ({0}), only accepts '
+            'integer values'.format(id(self)))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class GetSetList(GetSetParent):
+    def __init__(self, value=0):
+        self.vallist = [value]
+    def get_val(self):
+        return self.vallist[-1]
+    def get_vals(self):
+        return self.vallist
+    def set_val(self, value):
+        self.vallist.append(value)
+    def showdoc(self):
+        print('GetSetList object, len {0}, stores '
+            'history of values set'.format(self.vallist()))
