44af5e811bb0161189d028dd16fbf817ee69a408
Alek Mammadov <mamedxanli@gmail.com>
2017-04-15 21:34:15 +0200

    i0409

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
 #!/usr/bin/python
 
-import abc
+import random
+import StringIO
 
-class GetSetParent(object):
-    __metaclass__ = abc.ABCMeta
-    def __init__(self, value):
-        self.val = 0
-    def set_val(self, value):
-        self.val = value
-    def get_val(self):
-        return self.val
-    @abc.abstractmethod
-    def showdoc(self):
-        return
+class WriteMyStuff(object):
 
+    def __init__(self, writer):
+        self.writer = writer
 
-class GetSetInt(GetSetParent):
-    def set_val(self, value):
-        if not isinstance(value, int):
-            value = 0
-        super(GetSetInt, self).set_val(value)
-    def showdoc(self):
-        print('GetSetInt object ({0}), only accepts '
-            'integer values'.format(id(self)))
+    def write(self):
+        write_text = 'this is a silly message'
+        self.writer.write(write_text)
 
+fh = open('test.txt', 'w')
+w1 = WriteMyStuff(fh)
+w1.write()
+fh.close()
 
-class GetSetList(GetSetParent):
-    def __init__(self, value=0):
-        self.vallist = [value]
-    def get_val(self):
-        return self.vallist[-1]
-    def get_vals(self):
-        return self.vallist
-    def set_val(self, value):
-        self.vallist.append(value)
-    def showdoc(self):
-        print('GetSetList object, len {0}, stores '
-            'history of values set'.format(len(self.vallist)))
+sioh = StringIO.StringIO()
+w2 = WriteMyStuff(sioh)
+w2.write()
 
-x = GetSetInt(3)
-x.set_val(5)
-print(x.get_val())
-x.showdoc()
-
-gsl = GetSetList(5)
-gsl.set_val(10)
-gsl.set_val(20)
-print gsl.get_val()
-print gsl.get_vals()
-gsl.showdoc()
+print 'file object: ', open('test.txt', 'r').read()
+print "StringIO object: ", sioh.getvalue()
